8c463cf7025d8f2a7c3a093cf58161766caba399
minoru1206 <minono1206@gmail.com>
2020-05-23 21:12:19 +0900

    analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,22 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-167412453-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-167412453-1');
+</script>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="ゴロタンとは、英語学習を楽しく定着できるようゴロで覚えられる英単語クイズアプリです。受験や旅行、ビジネスなどにも活用していってくださいな！" />
+    <meta property="og:url" content="https://gorotan-english.work/" />
+    <meta property="og:description" content="ゴロタンとは、英語学習を楽しく定着できるようゴロで覚えられる英単語クイズアプリです。受験や旅行、ビジネスなどにも活用していってくださいな！" />
+    <meta property="og:image" content="/images/favicon.ico" />
     <meta name="google-site-verification" content="seNyghQr4dGx-JL0KXXW3RTOBFPshOHQaBSUwEE4aDo" />
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
