45f44f10392811589e9d259166d6c3657e8dd45b
minoru1206 <minono1206@gmail.com>
2020-06-06 08:04:06 +0900

    メタ要素

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,18 +13,20 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="ゴロタンとは、英語学習を楽しく定着できるようゴロで覚えられる英単語クイズアプリです。受験や旅行、ビジネスなどにも活用していってくださいな！" />
+    <meta name="description" content="簡単に英語を覚えられるように楽しくて面白い例文で覚えられる英単語のゴロとクイズのサイトです。英語学習を始める小学生、中学生から受験や旅行、ビジネスなどにも活用できます。楽しく英語を覚えていってくださいな！" />
     <meta property="og:url" content="https://gorotan-english.work/" />
-    <meta property="og:description" content="ゴロタンとは、英語学習を楽しく定着できるようゴロで覚えられる英単語クイズアプリです。受験や旅行、ビジネスなどにも活用していってくださいな！" />
-    <meta property="og:image" content="/images/header-logo.png" />
+    <meta property="og:description" content="簡単に英語を覚えられるように楽しくて面白い例文で覚えられる英単語のゴロとクイズのサイトです。英語学習を始める小学生、中学生から受験や旅行、ビジネスなどにも活用できます。楽しく英語を覚えていってくださいな！このサイトでは、単語の意味とその読み方と覚え方がある例文があるので、楽に英語学習ができます。" />
+    <meta property="og:image" content="https://www.gorotan-english.work/images/header-logo.png" />
     <meta name="google-site-verification" content="seNyghQr4dGx-JL0KXXW3RTOBFPshOHQaBSUwEE4aDo" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content=@gorotan_english">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
     <link href="/css/app.css" rel="stylesheet">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="api-token" content="{{ session()->get('api_token') ?? '' }}">
-    <title>ゴロタン ゴロで覚える英単語クイズ！</title>
+    <title>【英語】楽しく語呂で英語を覚えるための、面白い英単語のクイズ！</title>
     <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
 
   </head>
